c313eee34f0906e69924800a8a8a2b8c21cf2baf
Siddharth S Singh <simsid66@gmail.com>
2020-07-07 15:17:16 +0530

    reverted some breakage and added GameState class

---
+++ src/GameState.js
@@ -0,0 +1,24 @@
+class GameState {
+  constructor(jsonFileName) {
+    // could also be jsonStr
+    this.loadState(jsonFileName);
+    this.zones = [];
+    this.images = {}; // storage for images
+    this.players = {}; // an object containing player metadata (UID, maybe even client IP or something)
+    this.entities = {}; // Entity objects need to contain their position and other useful metadata such as png filename
+  }
+
+  loadState(fileName) {
+    // load state into this object
+  }
+
+  applyAction(zone, entity, action) {
+    
+  }
+
+  getCurrentStateInfo() {
+    // either this method or directly accessing the properties of this object
+  }
+}
+
+// Adding Zone and Entity classes might also be a good idea
