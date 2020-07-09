213eef3676fe4fc9b7051008f9c8acb498e14936
Zhenghong Lieu <lieu@pop-os.localdomain>
2020-07-09 22:37:21 +0200

    Build with tsc

--- built/Entity.js
+++ built/Entity.js
@@ -11,7 +11,23 @@ class Entity {
         this.zone = new_zone;
     }
     change_state(new_state) {
+        // TODO change image as well
         this.state = new_state;
+        /*
+        const entity_image_states = game.imageMap.image_mapping[entity.type];
+        // Looking through each of the entity states in the image map
+        entity_image_states.forEach((entity_img_state) => {
+        // Check that this entity state is identical
+        // this is a pretty bad approach because it scales with O(n^2) time
+        // TODO --- optimise this when we have time
+        if (_isEqual(entity.state, entity_img_state.state)) {
+        const entity_image_name = entity_img_state.image;
+        const image_blob = game.assets.images[entity_image_name];
+        createImageBitmap(image_blob).then((result) => game.ctx.drawImage(result, entity.pos[0], entity.pos[1]));
+        }
+        });
+        });
+        */
     }
     change_pos(new_pos) {
         this.pos = new_pos;
