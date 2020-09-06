b9eaa5120a4c1facad9b7f7a7160a5cdfbd2a2ad
Zhenghong Lieu <lieu@pop-os.localdomain>
2020-09-06 16:02:25 +0800

    add comments to drawMenu

--- src/Menu.ts
+++ src/Menu.ts
@@ -24,11 +24,16 @@ export class SubMenu extends Menu {
   }
 }
 
-export function drawMenu(menu: SubMenu, point: Point) {
+export function drawMenu(menu: SubMenu, point: Point): HTMLElement {
   // TODO
+  // This function should create a HTML element representing the
+  // menu object,
+  // and render that HTML element at position (x, y)
+  // Returns the HTML element
   console.log(menu);
   console.log(`Drawing menu at point (${point.x}, ${point.y})`);
 
+  let root_menu_DOM_elem: HTMLElement;
   return root_menu_DOM_elem;
 }
 
