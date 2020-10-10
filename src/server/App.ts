6000fc7205bd4c70bbfe82260b94014db6e068b4
Zhenghong Lieu <lieu@pop-os.localdomain>
2020-10-10 23:18:00 +0800

    Add server-side WebSocket

--- src/server/App.ts
+++ src/server/App.ts
@@ -3,12 +3,11 @@
 // handles lobbies/client input etc
 */
 
-const gameport = process.env.PORT || 4004;
-
-const io = require("socket.io");
 const express = require("express");
 const http = require("http");
+const webSocket = require("ws");
 
+const gameport = process.env.PORT || 4004;
 const __dirname__ = process.cwd();
 const app = express();
 const server = http.createServer(app);
@@ -18,16 +17,27 @@ app.get("/", (req, res) => {
   res.sendfile("/index.html", { root: __dirname__ });
 });
 
-app.listen(gameport, () => {
-  console.log(`Example app listening at http://localhost:${gameport}`);
-});
-
 // Serving static files
 app.get("/*", function (req, res, next) {
   //This is the current file they have requested
   let file = req.params[0];
   //For debugging, we can track what files are requested.
-  console.log("\t :: Express :: file requested : " + file);
+  // console.log("\t :: Express :: file requested : " + file);
   //Send the requesting client the file.
   res.sendfile(__dirname__ + "/" + file);
+});
+
+const wss = new webSocket.Server({ port: 4005 });
+wss.on("connection", (ws) => {
+  ws.on("message", (message: string) => {
+    ws.send(message); // Echo server
+    console.log("Message received!");
+  });
+
+  console.log("Connection established!");
+  ws.send("Connection established!");
+});
+
+app.listen(gameport, () => {
+  console.log(`Example app listening at http://localhost:${gameport}`);
 });
