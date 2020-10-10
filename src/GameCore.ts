cd085255c055d1f387e2808f33db31fb304e5cbf
Zhenghong Lieu <lieu@pop-os.localdomain>
2020-10-11 00:30:27 +0800

    Remove extraneous comments

--- src/GameCore.ts
+++ src/GameCore.ts
@@ -4,16 +4,6 @@ import { UIState, UIAction } from "./UI.js";
 import { ImageMap, Point, PlayerName, ServerAction } from "./Interfaces.js";
 import { drawMenu, MenuItem, SubMenu } from "./Menu.js";
 
-//The main update loop runs on requestAnimationFrame,
-//Which falls back to a setTimeout loop on the server
-//Code below is from Three.js, and sourced from links below
-
-// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-
-// requestAnimationFrame polyfill by Erik Möller
-// fixes from Paul Irish and Tino Zijdel
-
 var fixed_time_digits = 3;
 function fixed(value: number, digits: number) {
   return parseFloat(value.toFixed(digits));
@@ -21,45 +11,6 @@ function fixed(value: number, digits: number) {
 
 var frame_time = 60 / 1000; // run the local game at 16ms/ 60hz
 if ("undefined" != typeof global) frame_time = 45; //on server we run at 45ms, 22hz
-
-/*
-(function () {
-  var lastTime = 0;
-  var vendors = ["ms", "moz", "webkit", "o"];
-
-  for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-    window.requestAnimationFrame = window[vendors[x] + "RequestAnimationFrame"];
-    window.cancelAnimationFrame =
-      window[vendors[x] + "CancelAnimationFrame"] ||
-      window[vendors[x] + "CancelRequestAnimationFrame"];
-  }
-
-  if (!window.requestAnimationFrame) {
-    window.requestAnimationFrame = function (callback) {
-      var currTime = Date.now(),
-        timeToCall = Math.max(0, frame_time - (currTime - lastTime));
-      var id = window.setTimeout(function () {
-        callback(currTime + timeToCall);
-      }, timeToCall);
-      lastTime = currTime + timeToCall;
-      return id;
-    };
-  }
-
-  if (!window.cancelAnimationFrame) {
-    window.cancelAnimationFrame = function (id) {
-      clearTimeout(id);
-    };
-  }
-})();
-*/
-
-//Now the main game class. This gets created on
-//both server and client. Server creates one for
-//each game that is hosted, and client creates one
-//for itself to play the game.
-/*
- * The game_core class */
 
 class Timer {
   public dt: number = new Date().getTime();
@@ -86,7 +37,11 @@ class Timer {
   }
 }
 
-// I'm intending this to eventually be extended by GameClientCore and GameServerCore.
+//Now the main game class. This gets created on
+//both server and client. Server creates one for
+//each game that is hosted, and client creates one
+//for itself to play the game.
+
 abstract class GameCore {
   // GameCore members
   public game_state: GameState;
